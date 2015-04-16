725656ddb00f1ae9de876c42a7218e57118048aa
YuriyB <yuriybpost@gmail.com>
2015-04-16 08:27:26 +0200

    added entity Role

---
+++ src/main/java/com/yuriyb/petproject01/domain/Role.java
@@ -0,0 +1,39 @@
+package com.yuriyb.petproject01.domain; 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id")
+	private Long id;
+
+	@Column(name = "RoleName", length = 25, nullable = false)
+	private String roleName;
+
+	// Getters and setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
